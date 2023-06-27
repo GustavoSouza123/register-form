ea450dbca13c4e59422efb1b9a3a6ab6f17d8bab
Gustavo Souza <gustavoelia7@gmail.com>
2023-06-27 09:15:40 -0300

    initial commit: files, form container, styles

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css" />
+    <title>Formulário</title>
+</head>
+<body>
+    <div class="container">
+        <div class="form-title">
+            <h1>Cadastro</h1>
+        </div>
+        <form action="result.php" method="post">
+            <div class="data">
+                <div class="title">Dados gerais</div>
+            </div>
+            <div class="address">
+                <div class="title">Endereço</div>
+            </div>
+            <div class="languages">
+                <div class="title">Linguagens de programação</div>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
